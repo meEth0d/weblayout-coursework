d3d1d94bab8754f8a87ba9659666766ed048e177
Kovtun Ivan <komar_esic@mail.ru>
2021-04-08 16:59:47 +0300

    accordion

--- 13_javascript/js/swiper.js
+++ 13_javascript/js/swiper.js
@@ -1,21 +1,26 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+document.addEventListener("DOMContentLoaded", function() {
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      bulletClass: 'swiper-pagination-bullet section-hero__bullet',
+      bulletActiveClass: 'swiper-pagination-bullet-active section-hero__bullet-active'
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 });
--- 13_javascript/js/tabs.js
+++ 13_javascript/js/tabs.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.section-work__link').forEach(function(tabsBtn) {
-    tabsBtn.addEventListener('click', function(event) {
-      const path = event.currentTarget.dataset.path
+    tabsBtn.addEventListener('click', function(evt) {
+      const path = evt.currentTarget.dataset.path
 
 
       document.querySelectorAll('.section-work__content').forEach(function(tabContent) {
         tabContent.classList.remove('section-work__content-active')
       })
-      document.querySelector('[data-target="$path"]').classList.add('section-work__content-active')
+      document.querySelector(`[data-target="${path}"]`).classList.add('section-work__content-active')
     })
   })
 
